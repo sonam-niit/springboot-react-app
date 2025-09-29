4a14e26b63dfc1d0c4c389fc81ac60473d4a2a9f
sonam-niit <sonam.gravity@gmail.com>
2025-09-29 20:22:21 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  port: 8082,
+  server: {
+    host: true,      // required for Docker
+    port: 8082,      // change Vite port
+  },
 })
