837517723bef9213ffe78408c5b1037a8b87eb0c
sonam-niit <sonam.gravity@gmail.com>
2025-10-01 14:49:21 +0530

    Revert "port updated for frontend"
    This reverts commit f24d63791eeaa595fd6d4170ed224ec2c8b52036.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,      // required for Docker
+<<<<<<< HEAD
     port: 8100,      // change Vite port
     allowedHosts: true
+=======
+    port: 8082,      // change Vite port
+>>>>>>> parent of f24d637 (port updated for frontend)
   },
 })
